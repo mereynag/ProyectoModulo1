11260b1de2ec52d25d462a5e07f54c735f1f73a7
Maria Reyna <mereynag@gmail.com>
2020-10-28 15:46:32 -0400

    Avances en el home

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ let flyObjs = []
 class Board{
     constructor(){
     this.x = 0
-    this.y = -1000
+    this.y = -4300
     this.width = $canvas.width
     this.height = 5000
     this.img = new Image()
